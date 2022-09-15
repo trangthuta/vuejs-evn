6c84561d5577e57c1a18c9217434753f820555aa
Tạ Thị Thu Trang <trangttt@edsolabs.com>
2022-09-15 18:19:26 +0700

    push lan 2

--- src/views/formHs/FormHS.vue
+++ src/views/formHs/FormHS.vue
@@ -5,9 +5,41 @@
     </p>
 
     <form>
-      <div>
-        
-      </div>
+      <table class="home-table">
+        <tr>
+          <td>
+            <p>
+              Đơn vị thực hiện
+            </p>
+            <input type="text" value="Công ty nhiệt điện Phú Mỹ"/>
+          </td>
+          <td>
+            <p>
+              Chuyên viên thực hiện
+            </p>
+            <section>
+              <option value="01">
+                Nguyễn Văn A
+              </option>
+              <option>
+                Nguyễn Văn B
+              </option>
+            </section>
+          </td>
+          <td>
+            <input type="text" placeholder="Tìm kiếm..." />
+          </td>
+        </tr>
+        <tr>
+          <td>dd</td>
+          <td>
+            <input type="text" placeholder="Tìm kiếm......" />
+          </td>
+          <td>
+            <input type="text" placeholder="Tìm kiếm..." />
+          </td>
+        </tr>
+      </table>
     </form>
   </div>
 </template>
@@ -22,5 +54,4 @@ export default {
 .hoso {
   background-color: #fff;
 }
-
 </style>
